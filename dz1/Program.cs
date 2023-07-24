6f3ce8602dd3be4a8f91bbbb489fd9d7e3f7b853
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-07-24 22:13:54 +0300

    Начало написание программы

---
+++ dz1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 2. Программа на вход принимает два числаи выдает, какое число больше, а какое меньше
+Console.WriteLine("Hello, World!");
+
