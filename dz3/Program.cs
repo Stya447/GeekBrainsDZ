bd02bc851f67659ab4f55653e48976dfe76cc8c5
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-07-24 23:37:10 +0300

    Написание 6 задачи

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 6. Программа, которая на вход принимает число и выдает, является ли оно четным
+Console.WriteLine("Введите число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+if (number1 % 2 == 0)
+{
+    Console.WriteLine($"Число {number1} является четным");
+}
+else 
+{
+    Console.WriteLine($"Число {number1} не является четным");
+}
