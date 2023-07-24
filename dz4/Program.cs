aa8c17a2cdf11a56edb8d941990fad284161db66
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-07-24 23:46:38 +0300

    Выполнение 8 задачи

--- dz4/Program.cs
+++ dz4/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 8. Программа, которая  на вход принимает число, а на выходе показывает все четные числа до введенного числа
+Console.WriteLine("Введите число");
+string userInput1 = Console.ReadLine() ?? "";
+int number = int.Parse(userInput1);
+int numbers = 2;
+while (numbers <= number)
+{
+    Console.Write($"{numbers} ");
+    numbers = numbers+2;
+}
