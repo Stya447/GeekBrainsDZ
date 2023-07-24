89954209dff1658ffe09382860ce2e1d93510be8
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-07-24 22:40:44 +0300

    Выполнена 2 задача

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,4 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Задача 2. Программа на вход принимает два числаи выдает, какое число больше, а какое меньше
-Console.WriteLine("Hello, World!");
-
+Console.WriteLine("Введите два числа:");
+string userInput1 = Console.ReadLine() ?? "";
+string userInput2 = Console.ReadLine() ?? "";
+int number1 = int.Parse(userInput1);
+int number2 = int.Parse(userInput2);
+int max = 0;
+int min = 0;
+if (number1 == number2)
+{
+    Console.WriteLine("Вы ввели одинаковые числы, максимального и минимального не обнаружено");
+}
+else
+{
+if (number1 > number2)
+{
+    max = number1;
+    min = number2;
+}
+else 
+{
+    max = number2;
+    min = number1;
+}
+Console.WriteLine($"max = {max} min = {min}");
+}
