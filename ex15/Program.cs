8c3c5b63f0deb61a2acb92f555dd74d6a49b85d5
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-07-25 23:42:53 +0300

    Решение 15 задачи

--- ex15/Program.cs
+++ ex15/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 15. Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите чичсло от 1 до 7");
+string userInput1 = Console.ReadLine() ?? "";
+int number = int.Parse(userInput1);
+if (number <= 7)
+{
+    if ((number == 6) || (number == 7))
+    {
+        Console.WriteLine($"{number} -> является выходным");
+    }
+    else
+    {
+        Console.WriteLine($"{number} -> не является выходным");
+    }
+
+}
+else
+{
+    Console.WriteLine("Читайте внимательно и введите, пожалуйста, число занаво");
+}
