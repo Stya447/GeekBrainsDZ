ac90d7459d386fb59df2e627181c2b6c9308c6ac
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-07-25 23:04:51 +0300

    Решение 10 задачи

--- ex10/Program.cs
+++ ex10/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 10. Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int number = int.Parse(userInput1);
+int a1 = 0;
+int a2 = 0;
+a1 = number % 100;
+a2 = a1 / 10;
+Console.WriteLine($"{number} -> {a2}");
