f593339f8a1dc47943e54e6c7c5793908a314e45
Anastasia Akhmetova <asyaphilip447@gmail.com>
2023-07-25 23:34:50 +0300

    Создание нового файла для 15 задачи

--- ex13/Program.cs
+++ ex13/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Задача 13. Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+string userInput1 = Console.ReadLine() ?? "";
+int number = int.Parse(userInput1);
+if (number > 99)
+{
+    var some = number.ToString();
+    Console.WriteLine($"{number} -> {some[2]}");
+}
+else{
+{
+    Console.WriteLine($"{number} -> третьей цифры нет");
+} }
